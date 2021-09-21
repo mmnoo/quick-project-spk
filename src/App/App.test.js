0f260e8483515b4eac13eb8093b92d467351652b
Melissa Nunes <mnunes@sparkgeo.com>
2021-09-21 12:21:49 -0600

    Make sure Husky actually works

---
+++ src/App/App.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import App from './App'
+
+test('Im just a test so Husky lets us push to origin. Well delete me soon when real tests exist', () => {
+  render(<App />)
+})
